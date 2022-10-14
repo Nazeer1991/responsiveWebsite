ecdc63664db0bb1c0d4e5f307281be24f565a0a5
nazeersathak <nazeersathak@gmail.com>
2022-10-14 22:34:31 +0530

    added few files

---
+++ watsap.py
@@ -0,0 +1,12 @@
+# import pyautogui
+# import time
+# time.sleep(4)
+# count =0
+# while count <= 3 :
+#     pyautogui.typewrite('hi')
+#     pyautogui.press('enter')
+#     count = count + 1
+
+
+# import pywhatkit
+# pywhatkit.sendwhatmsg('+919789522181','Hi',9,18)
